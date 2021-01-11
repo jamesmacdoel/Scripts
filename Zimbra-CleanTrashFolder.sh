f7d550ace7be7f1382bd794fe01ac7e688d11a3e
jamesmacdoel <junior@syma.com.br>
2021-01-11 18:42:50 -0300

    Bash script to clean trash / junk folder from all zimbra users

--- Zimbra-CleanTrashFolder.sh
+++ Zimbra-CleanTrashFolder.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Clean junk folder from all users in zimbra
+# Clean trash/Junk folder from all users in zimbra
 
 /opt/zimbra/bin/zmprov -l gaa | while read line ; do
         echo Cleaning $line
+        #/opt/zimbra/bin/zmmailbox -z -m $line emptyFolder /Junk
         /opt/zimbra/bin/zmmailbox -z -m $line emptyFolder /Trash
 done
